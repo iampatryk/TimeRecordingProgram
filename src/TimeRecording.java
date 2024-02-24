66022a303d57bad979f7b5e44c41e9018b391f45
iampatryk <mpatrykmiller@gmail.com>
2024-02-24 10:29:50 +0100

    Update Project

---
+++ src/TimeRecording.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TimeRecording {
+
+    void getNumberIdFromUserAndSaveTimeAndDate() {
+        //Pobieranie numeru ID od uzytkownika
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your ID number:  ");
+        int userID = scanner.nextInt();
+
+
+
+    }
+}
