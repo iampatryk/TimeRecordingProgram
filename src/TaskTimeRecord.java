465454e004202488efa625af23e7db42137738ea
iampatryk <mpatrykmiller@gmail.com>
2024-02-26 20:15:28 +0100

    validation cardNumber

--- src/TaskTimeRecord.java
+++ src/TaskTimeRecord.java
@@ -3,6 +3,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class TaskTimeRecord {
@@ -10,11 +12,13 @@ public class TaskTimeRecord {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome!");
 
+        int[] cardNumber = {1212, 2323, 3434, 4545, 2222, 3333};
+
         while (true) {
-            System.out.println("Entry your Job Card number or 'exit' to close program.");
+            System.out.println("Entry your Job Card number or 'exit','end' to close program.");
             String jobCardString = scanner.nextLine();
 
-            if (jobCardString.toLowerCase().equals("exit")) {
+            if (jobCardString.toLowerCase().equals("exit") || jobCardString.toLowerCase().equals("end") ) {
                 break;
             }
 
@@ -25,6 +29,12 @@ public class TaskTimeRecord {
                 System.out.println("Entry correct number");
                 continue;
             }
+
+            if(!isValidCardNumber(jobCardNumber, cardNumber)) {
+                System.out.println("Can't find this Job Card number. Try again.");
+                continue;
+            }
+
             System.out.println("Press Enter to start your job");
             scanner.nextLine();
 
@@ -50,10 +60,19 @@ public class TaskTimeRecord {
 
     private void saveToFile(int jobCardNumber, long timeStart, long timeStop, long timeCalc) {
         try(BufferedWriter writer = new BufferedWriter(new FileWriter("timeRec.txt",true))) {
-            writer.write("Job Card Number: " + jobCardNumber + " | Time Start: " + timeStart + " | Time Stop: " + timeStop + " | Time: " + (timeStop - timeStart) + " | TIME: " + timeCalc + "\n");
+            writer.write("Job Card Number: " + jobCardNumber + " | Time Start: " + timeStart + " | Time Stop: " + timeStop + " | Time: " + (timeStop - timeStart) + " | TIME: " + timeCalc + " second" + "\n");
 
         } catch (IOException e) {
             System.out.println("Error !");
         }
+    }
+
+    private boolean isValidCardNumber(int jobCardNumber, int[] cardNumber) {
+        for(int validJobCardNumber : cardNumber) {
+            if (jobCardNumber == validJobCardNumber) {
+                return true;
+            }
+        }
+        return false;
     }
 }
